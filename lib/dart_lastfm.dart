e59be71e4ff1b29a91a3e497d5bfb42e0feb9345
Mark Music <markmusic999@gmail.com>
2021-01-04 08:55:42 -0600

    ft[services/network] =>  🌎 Add network services to package with handle methods and futures

--- lib/dart_lastfm.dart
+++ lib/dart_lastfm.dart
@@ -1,7 +1,3 @@
 library dart_lastfm;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ lib/service/network.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkService {
+  final String endPoint;
+  NetworkService({@required this.endPoint});
+
+  Future<String> retrieveCall() async {
+    http.Response response = await http.get(endPoint);
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw (response.statusCode);
+    }
+  }
+}
