152b9cea2c133066f9773b1deb4797b4e6454416
Mark Music <markmusic999@gmail.com>
2021-01-04 09:36:33 -0600

    ft[lib/dart_lastfm.dart] => 🖍 Add getData method with jsonDecode

--- lib/dart_lastfm.dart
+++ lib/dart_lastfm.dart
@@ -1,3 +1,36 @@
 library dart_lastfm;
 
+import 'dart:convert';
+
+import 'package:dart_lastfm/models/stream.dart';
+import 'package:dart_lastfm/services/network.dart';
+import 'package:flutter/material.dart';
+
 /// A Calculator.
+class LastFM {
+  final String username;
+  final String key;
+
+  LastFM({@required this.key, @required this.username});
+
+  Future<String> _callData() async {
+    NetworkService networkService = NetworkService(
+      endPoint:
+          "ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$username&api_key=$key&format=json&limit=1",
+    );
+
+    return await networkService.retrieveCall();
+  }
+
+  Future<StreamModel> getData() async {
+    final String data = await _callData();
+
+    StreamModel model = StreamModel(
+      artist: jsonDecode(data)["recenttracks"]["tracks"][0]["artist"]["#text"],
+      coverArt: jsonDecode(data)["recenttracks"]["tracks"][0]["image"][3]
+          ["#text"],
+    );
+
+    print(model);
+  }
+}
---
+++ lib/models/stream.dart
@@ -0,0 +1,13 @@
+class StreamModel {
+  final String coverArt;
+  final String name;
+  final String url;
+  final String artist;
+
+  StreamModel({
+    this.artist,
+    this.coverArt,
+    this.name,
+    this.url,
+  });
+}
